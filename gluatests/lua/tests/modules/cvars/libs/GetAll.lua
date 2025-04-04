8d1ce38a4fe144048a5dd3553d0e7ffe0540ac33
FirstLemon <obivannix@gmail.com>
2025-04-04 14:48:58 +0200

    fixed typos

--- gluatests/lua/tests/modules/cvars/libs/GetAll.lua
+++ gluatests/lua/tests/modules/cvars/libs/GetAll.lua
@@ -1,23 +1,23 @@
 return {
-    groupName = "CopyReadBuffer",
+    groupName = "cvars",
     cases = {
         {
             name = "Function exists globally",
-            when = HolyLib_IsModuleEnabled("cvar"),
+            when = HolyLib_IsModuleEnabled("cvars"),
             func = function()
-                expect( CreateEntityList ).to.beA( "function" )
+                expect( FindMetaTable("GetAll") ).to.beA( "function" )
             end
         },
         {
             name = "Function doesn't exists globally",
-            when = not HolyLib_IsModuleEnabled("cvar"),
+            when = not HolyLib_IsModuleEnabled("cvars"),
             func = function()
-                expect( CreateEntityList ).to.beA( "nil" )
+                expect( FindMetaTable("GetAll") ).to.beA( "nil" )
             end
         },
         {
             name = "Returns an Table object",
-            when = HolyLib_IsModuleEnabled("cvar"),
+            when = HolyLib_IsModuleEnabled("cvars"),
             func = function()
                 local cvar_list = {}
                 expect( cvar_list ).to.beA( "table" )
