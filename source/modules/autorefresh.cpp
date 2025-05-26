cf71461f6135fdafe0133afb54372caf80d72447
FirstLemon <obivannix@gmail.com>
2025-05-26 13:10:46 +0200

    autorefresh: experimentation
    - the usual brainrot

--- source/modules/autorefresh.cpp
+++ source/modules/autorefresh.cpp
@@ -40,28 +40,6 @@ static void hook_CAutoRefresh_HandleLuaFileChange(const std::string *fileRelPath
 	}
 };
 
-
-static Detouring::Hook detour_CAutoRefresh_GetChange;
-static void hook_CAutoRefresh_GetChange(std::string *strName)
-{
-	if (!g_Lua) {
-		return;
-	}
-
-	if (Lua::PushHook("HolyLib:GetChange"))
-	{
-		g_Lua->PushString(strName->c_str());
-		g_Lua->CallFunctionProtected(2, 0, true);
-	}
-};
-
-
-static Detouring::Hook detour_CAutoRefresh_CheckForChanges;
-static void hook_CAutoRefresh_CheckForChanges()
-{
-
-};
-
 void CAutoRefreshModule::LuaInit(GarrysMod::Lua::ILuaInterface* pLua, bool bServerInit)
 {
 	if (bServerInit)
@@ -88,20 +66,6 @@ void CAutoRefreshModule::InitDetour(bool bPreServer)
 		&detour_CAutoRefresh_HandleLuaFileChange, "CAutoRefresh_HandleLuaFileChange",
 		server_loader.GetModule(), Symbols::GarrysMod_AutoRefresh_HandleLuaFileChangeSym,
 		(void*)hook_CAutoRefresh_HandleLuaFileChange, m_pID
-	);
-
-	// Bootil::ChangeMonitor::GetChange
-	Detour::Create(
-		&detour_CAutoRefresh_GetChange, "CAutoRefresh_GetChangeSym",
-		server_loader.GetModule(), Symbols::Bootil_File_ChangeMonitor_GetChangeSym,
-		(void *)hook_CAutoRefresh_GetChange, m_pID
-	);
-
-	// Bootil::ChangeMonitor::CheckForChanges
-	Detour::Create(
-		&detour_CAutoRefresh_CheckForChanges, "CAutoRefresh_CheckForChanges",
-		server_loader.GetModule(), Symbols::Bootil_File_ChangeMonitor_CheckForChangesSym,
-		(void *)hook_CAutoRefresh_CheckForChanges, m_pID
 	);
 }
 
