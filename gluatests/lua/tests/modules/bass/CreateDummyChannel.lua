cfc86018448434fecf20c396712881006d7c0b80
FirstLemon <obivannix@gmail.com>
2025-11-29 10:27:38 +0100

    tests: added CreateDummyChannel tests

---
+++ gluatests/lua/tests/modules/bass/CreateDummyChannel.lua
@@ -0,0 +1,71 @@
+return {
+    groupName = "bass.CreateDummyChannel",
+
+    cases = {
+        {
+            name = "Function exists when module enabled",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                expect( bass.CreateDummyChannel ).to.beA( "function" )
+            end
+        },
+        {
+            name = "Function is nil when module disabled",
+            when = not HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                expect( bass ).to.beA( "nil" )
+            end
+        },
+        {
+            name = "Returns a valid IGModAudioChannel and nil error for valid call",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                local dummyChannel, err = bass.CreateDummyChannel( 44000, 2, 0 )
+
+                expect( dummyChannel ).to.exist()
+                expect( dummyChannel ).to.beA( "IGModAudioChannel [DUMMY]" )
+                expect( err ).to.beNil()
+            end
+        },
+        {
+            name = "Testing function with invalid sample rate",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                local dummyChannel, err = bass.CreateDummyChannel( -1, 1, 0 )
+
+                expect( dummyChannel ).to.beNil()
+                expect( err ).to.beA( "string" )
+                expect( err ).to.equal( "BASS_ERROR_FORMAT" )
+            end
+        },
+        {
+            name = "Testing function with invalid channel count",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                local dummyChannel, err = bass.CreateDummyChannel( 44000, 0, 0 )
+
+                expect( dummyChannel ).to.beNil()
+                expect( err ).to.beA( "string" )
+                expect( err ).to.equal( "BASS_ERROR_FORMAT" )
+            end
+        },
+        {
+            name = "Testing function with invalid flag parameter",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                local dummyChannel, err = bass.CreateDummyChannel( 44000, 1, -1 )
+
+                expect( dummyChannel ).to.beNil()
+                expect( err ).to.beA( "string" )
+                expect( err ).to.equal( "BASS_ERROR_NO3D" )
+            end
+        },
+        {
+            name = "Invalid arg types throw lua error",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                expect( bass.CreateDummyChannel, "We are not horses", 1, 0 ).to.err()
+            end
+        },
+    }
+}
