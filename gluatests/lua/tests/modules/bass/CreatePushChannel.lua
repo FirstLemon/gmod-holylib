2048db7112e3dd780a8d9c66380beae462a1f168
FirstLemon <obivannix@gmail.com>
2025-11-29 10:45:01 +0100

    tests: added CreatePushChannel tests

---
+++ gluatests/lua/tests/modules/bass/CreatePushChannel.lua
@@ -0,0 +1,60 @@
+return {
+    groupName = "bass.CreateDummyChannel",
+
+    cases = {
+        {
+            name = "Function exists when module enabled",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                expect( bass.CreatePushChannel ).to.beA( "function" )
+            end
+        },
+        {
+            name = "Function is nil when module disabled",
+            when = not HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                expect( bass ).to.beA( "nil" )
+            end
+        },
+        {
+            name = "Returns a valid igmodaudiochannel and nil for a valid call",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                local channel, err = bass.CreatePushChannel( 44100, 2, 0 )
+
+                expect( channel ).to.exist()
+                expect( channel ).to.beA( "userdata" )
+                expect( err ).to.beNil()
+            end
+        },
+        {
+            name = "Invalid sample rate returns nil and BASS_ERROR_FORMAT",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                local channel, err = bass.CreatePushChannel( -1, 2, 0 )
+
+                expect( channel ).to.beNil()
+                expect( err ).to.beA( "string" )
+                expect( err ).to.equal( "BASS_ERROR_FORMAT" )
+            end
+        },
+        {
+            name = "Invalid channel count returns nil and BASS_ERROR_FORMAT",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                local channel, err = bass.CreatePushChannel( 44100, 0, 0 )
+
+                expect( channel ).to.beNil()
+                expect( err ).to.beA( "string" )
+                expect( err ).to.equal( "BASS_ERROR_FORMAT" )
+            end
+        },
+        {
+            name = "Invalid arg types throw lua error",
+            when = HolyLib_IsModuleEnabled( "bass" ),
+            func = function()
+                expect( bass.CreatePushChannel, "44000", 1, 0 ).to.err()
+            end
+        },
+    }
+}
